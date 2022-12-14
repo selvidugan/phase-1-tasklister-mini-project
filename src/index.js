cfd16bf9db9e5ac0b7417e5ed1c855694788b52a
Selvi Ece Dugan <selvidugan@gmail.com>
2022-12-14 10:27:27 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const input = document.getElementById("new-task-description");
+  const form = document.querySelector("form");
+  form.addEventListener('submit', (e) => {
+    let newtask = input.value
+    e.preventDefault();
+    if (newtask.length === 0) {
+      alert('Task cannot be empty')
+    } else {
+      handleToDo(newtask);
+    }
+    form.reset()
+  })
 });
+
+
+function handleToDo(newtask) {
+  let listItem = document.createElement("li");
+  let btn = document.createElement('button')
+
+
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'X Remove'
+  listItem.innerText = `${newtask}`;
+  listItem.appendChild(btn)
+  document.querySelector("#tasks").appendChild(listItem);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
